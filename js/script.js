11a7270ee15ea8a599c9e7bf221a7fc631d25519
Jose Guadalupe Vidal Anaya <joseguadalupevidalanaya@gmail.com>
2018-05-09 14:28:20 -0500

    agregando validaciones  de registro

--- js/script.js
+++ js/script.js
@@ -37,10 +37,7 @@ $num.on("keyup click ready", function (event)
         $num.removeClass('invalid').addClass('valid');
     else
         $num.removeClass('valid').removeClass('invalid');
-    if ($num.hasClass('valid') && $pass.hasClass('valid'))
-        $enviar.removeClass('disabled');
-    else
-        $enviar.addClass('disabled');
+    activarEnv();
 });
 
 $pass.on("keyup click ready", function (event)
@@ -49,10 +46,7 @@ $pass.on("keyup click ready", function (event)
         $pass.addClass('valid').removeClass('invalid');
     else
         $pass.removeClass('valid').removeClass('invalid');
-    if ($num.hasClass('valid') && $pass.hasClass('valid'))
-        $enviar.removeClass('disabled');
-    else
-        $enviar.addClass('disabled');
+    activarEnv();
 });
 
 $pass.on("blur", function ()
@@ -61,6 +55,17 @@ $pass.on("blur", function ()
         $pass.addClass('invalid').removeClass('valid');
 });
 
+function activarEnv()
+{
+    if ($num.hasClass('valid') && $pass.hasClass('valid'))
+        $enviar.removeClass('disabled');
+    else
+        $enviar.addClass('disabled');
+}
+
+/**
+ * Enviar datos y recibir respruesta
+ */
 $enviar.click(function ()
 {
     var x = $pass.val(), y = $num.val();
@@ -77,3 +82,107 @@ $enviar.click(function ()
             location.href = 'php/inicio.php';
     });
 });
+
+/**
+ * Validar el registro
+ */
+
+var $nombre = $('#nombre');
+var $apellido = $('#apellido');
+var $control = $('#control');
+var $pass1 = $('#pass');
+var $pass2 = $('#pass2');
+var $crear = $('#crear');
+
+$nombre.on("keyup click ready", function (event)
+{
+    if ($nombre.val().length > 0)
+        $nombre.addClass('valid').removeClass('invalid');
+    else
+        $nombre.removeClass('valid').removeClass('invalid');
+    activarCrear();
+});
+
+$nombre.on("blur", function ()
+{
+    if ($nombre.val().length <= 0)
+        $nombre.addClass('invalid').removeClass('valid');
+});
+
+$apellido.on("keyup click ready", function (event)
+{
+    if ($apellido.val().length > 0)
+        $apellido.addClass('valid').removeClass('invalid');
+    else
+        $apellido.removeClass('valid').removeClass('invalid');
+    activarCrear();
+});
+
+$apellido.on("blur", function ()
+{
+    if ($apellido.val().length <= 0)
+        $apellido.addClass('invalid').removeClass('valid');
+});
+
+$control.on("keypress paste", function (event)
+{
+    if (this.value.length >= this.maxLength)
+        event.preventDefault();
+});
+
+$control.on("blur", function ()
+{
+    if ($control.val().length != 13)
+        $control.removeClass('valid').addClass('invalid');
+});
+
+$control.on("keyup click ready", function (event)
+{
+    if ($control.val().length == 13)
+        $control.removeClass('invalid').addClass('valid');
+    else
+        $control.removeClass('valid').removeClass('invalid');
+    activarCrear();
+});
+
+$pass1.on("keyup click ready", function (event)
+{
+    if ($pass1.val().length > 0)
+        $pass1.addClass('valid').removeClass('invalid');
+    else
+        $pass1.removeClass('valid').removeClass('invalid');
+    if ($pass1.val() != $pass2.val() && $pass2.val() > 0)
+        $pass2.addClass('invalid').removeClass('valid');
+    activarCrear();
+});
+
+$pass1.on("blur", function ()
+{
+    if ($pass1.val().length <= 0)
+        $pass1.addClass('invalid').removeClass('valid');
+});
+
+$pass2.on("keyup click ready", function (event)
+{
+    if ($pass2.val().length > 0 && $pass1.val() == $pass2.val())
+        $pass2.addClass('valid').removeClass('invalid');
+    else
+        $pass2.removeClass('valid').removeClass('invalid');
+    activarCrear();
+});
+
+$pass2.on("blur", function ()
+{
+    if ($pass2.val().length <= 0 || $pass1.val() != $pass2.val())
+        $pass2.addClass('invalid').removeClass('valid');
+});
+
+function activarCrear()
+{
+    if ($nombre.hasClass('valid') && $apellido.hasClass('valid') && $pass1.hasClass('valid') && $pass2.hasClass('valid') && $control.hasClass('valid'))
+        $crear.removeClass('disabled');
+    else
+        $crear.addClass('disabled');
+}
+
+
