3ca7f7f62d691ace9fc455ae42dc6bdb45a7af61
Jose Guadalupe Vidal Anaya <joseguadalupevidalanaya@gmail.com>
2018-05-21 19:37:19 -0500

    zxcvbnm,

--- php/carreraAl.php
+++ php/carreraAl.php
@@ -5,3 +5,18 @@
      * Date: 10/05/18
      * Time: 02:28 PM
      */
+    include_once "conexion.php";
+    function carrera($car)
+    {
+        return ['carrera' => $car];
+    }
+
+    $usr = $_GET['usr'];
+
+    $cons = "SELECT id_carrera FROM alumno WHERE no_control='$usr'";
+
+    $gsent = $con->prepare($cons);
+    $gsent->execute();
+
+    $resultado = $gsent->fetchAll(PDO::FETCH_FUNC, "carrera");
+    echo json_encode($resultado);
